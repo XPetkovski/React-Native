6171b8a9904c21140e69031549edd2a1fb031f7e
XPetkovski <57604843+XPetkovski@users.noreply.github.com>
2022-06-09 11:27:29 +0200

    ?

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ListItem from "./app/components/ListItem";
+import MessagesScreen from "./app/screens/MessagesScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
+
+export default function App() {
+    return (
+        // <LoginScreen/>
+        // <MessagesScreen/>
+        <ListingEditScreen/>
+    );
+}
+
+
